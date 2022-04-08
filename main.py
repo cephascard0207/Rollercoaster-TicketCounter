7ad3dc1c349d65b82db2637e6239eaadf93ccd6b
Cephas Cardozo <95212186+cephascardozo@users.noreply.github.com>
2022-04-08 04:36:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Rollercoaster-TicketCounter
+#Created by Cephas Cardozo using Python
+#Day-3 of 100DaysCodingChallenge
+
+print("Welcome to the Rollercoaster")
+height = int(input("What is your height in cms?\n"))
+
+if height > 180:
+  print("You can ride the rollercoaster")
+else:
+  print("Sorry, You cannot ride the rollercoaster")
