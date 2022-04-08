280c1fea7986fc7ee49d228baf4283c83cacd85f
Cephas Cardozo <95212186+cephascardozo@users.noreply.github.com>
2022-04-08 05:04:02 +0000

    if/elif/else Statements rearranged with code blocks.

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 #Created by Cephas Cardozo using Python
 #Day-3 of 100DaysCodingChallenge
 
-print("Welcome to the Rollercoaster")
+print("Welcome to the Rollercoaster\n")
 height = int(input("What is your height in cms?\n"))
+age = int(input("What is your age?\n"))
 
 if height >= 120:
   print("You can ride the rollercoaster")
+
+  #Nested if/else Statements
+  if age <= 12:
+    print("You have to pay $5")
+  elif age <= 18:
+    print("You have to pay $7 ")
+  else:
+    print("You have to pay $12")
+    
 else:
   print("Sorry, You cannot ride the rollercoaster")
