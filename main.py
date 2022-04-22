5c0112c5f7e81f0c86bc2c2b51383e2044f9096e
Cephas Cardozo <95212186+cephascardozo@users.noreply.github.com>
2022-04-22 04:07:20 +0000

    Added "and" logical operator

--- main.py
+++ main.py
@@ -5,17 +5,28 @@
 print("Welcome to the Rollercoaster\n")
 height = int(input("What is your height in cms?\n"))
 age = int(input("What is your age?\n"))
+bill = 0
 
 if height >= 120:
   print("You can ride the rollercoaster")
 
   #Nested if/else Statements
   if age <= 12:
+    bill = 5
     print("You have to pay $5")
   elif age <= 18:
+    bill = 7 
     print("You have to pay $7 ")
+  elif age >= 45 and age <= 55:
+    print("Everything is gonna be OK. Have a free Ride")
   else:
+    bill = 12
     print("You have to pay $12")
-    
+
+  wants_photo = input("Do you want a photo? Y or N\n")
+  if wants_photo == "Y":
+    bill += 3
+  print(f"Your bill is ${bill}")
+  
 else:
   print("Sorry, You cannot ride the rollercoaster")
